4b9a6eafae0a3c5fafe4931ee5a3f9933e1b42cc
Dione Santos <dionedev@gmail.com>
2024-10-10 19:44:40 -0300

    feat: adiciona tela de detalhes do produto

--- lib/ui/modules/product_details/product_details_page.dart
+++ lib/ui/modules/product_details/product_details_page.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_garden/shared/utils/app_colors.dart';
+import '../product_details/components/plant_info.dart';
 
 class ProductDetailsPage extends StatefulWidget {
   const ProductDetailsPage({super.key});
@@ -23,8 +23,8 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
           onPressed: () {},
           icon: SvgPicture.asset(
             'lib/ui/assets/icons/arrowleft.svg',
-            width: 24,
-            height: 24,
+            width: 20,
+            height: 20,
           ),
         ),
         actions: [
@@ -32,13 +32,17 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
             onPressed: () {},
             icon: SvgPicture.asset(
               'lib/ui/assets/icons/favorite.svg',
-              width: 24,
-              height: 24,
+              width: 20,
+              height: 20,
             ),
           ),
           IconButton(
             onPressed: () {},
-            icon: SvgPicture.asset('lib/ui/assets/icons/share.svg'),
+            icon: SvgPicture.asset(
+              'lib/ui/assets/icons/share.svg',
+              width: 20,
+              height: 20,
+            ),
           ),
         ],
       ),
@@ -47,11 +51,12 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
           children: [
             Image.asset(
               'lib/ui/assets/images/faux-watermelon-peperomia-plant-gray-pot.png',
+              height: 300,
+              width: 300,
               fit: BoxFit.cover,
             ),
             Container(
               padding: const EdgeInsets.all(20),
-              height: 457,
               width: double.infinity,
               decoration: const BoxDecoration(
                 color: AppColors.cardBackgroundColor,
@@ -67,35 +72,46 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                     'Peperomia Flex',
                     style: TextStyle(
                       fontSize: 20,
+                      color: AppColors.primaryTextColor,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Row(
-                    children: [
-                      SvgPicture.asset(
-                        'lib/ui/assets/icons/yellowratingstar.svg',
-                      ),
-                      SvgPicture.asset(
-                        'lib/ui/assets/icons/yellowratingstar.svg',
-                      ),
-                      SvgPicture.asset(
-                        'lib/ui/assets/icons/yellowratingstar.svg',
-                      ),
-                      SvgPicture.asset(
-                        'lib/ui/assets/icons/yellowratingstar.svg',
-                      ),
-                      SvgPicture.asset(
-                        'lib/ui/assets/icons/greyratingstar.svg',
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 4),
-                        child: Text('4.0'),
-                      ),
-                      const Text('(146 Reviews)'),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 9.5, bottom: 24.5),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          'lib/ui/assets/icons/yellowratingstar.svg',
+                        ),
+                        SvgPicture.asset(
+                          'lib/ui/assets/icons/yellowratingstar.svg',
+                        ),
+                        SvgPicture.asset(
+                          'lib/ui/assets/icons/yellowratingstar.svg',
+                        ),
+                        SvgPicture.asset(
+                          'lib/ui/assets/icons/yellowratingstar.svg',
+                        ),
+                        SvgPicture.asset(
+                          'lib/ui/assets/icons/greyratingstar.svg',
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 4),
+                          child: Text(
+                            '4.0',
+                            style: TextStyle(color: AppColors.primaryTextColor),
+                          ),
+                        ),
+                        const Text(
+                          '(146 Reviews)',
+                          style: TextStyle(color: AppColors.primaryTextColor),
+                        ),
+                      ],
+                    ),
                   ),
                   Text(
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci sem feugiat ut nullam nisl orci, volutpat, felis. Nunc elit, et mattis commodo condimentum tellus et. Dolor sit amet, consectetur adipiscing elit. Orci sem feugiat ut nullam nisl orci.',
+                    style: const TextStyle(color: AppColors.primaryTextColor),
                     maxLines: isExpanded ? null : 4,
                     overflow: isExpanded
                         ? TextOverflow.visible
@@ -108,185 +124,172 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                       });
                     },
                     child: Text(
-                      isExpanded ? 'Ler menos' : 'Ler mais',
+                      isExpanded ? 'Read less' : 'Read more',
                       style: const TextStyle(
                         color: AppColors.primaryGreenColor,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ),
-                  // TextButton(
-                  //   onPressed: () {},
-                  //   child: Text(isExpanded ? 'Ler menos' : 'Ler mais'),
-                  // ),
-                  const Divider(),
-                  Row(
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Divider(
+                      color: Color(0xFFDBDAD9),
+                      thickness: 0.9,
+                    ),
+                  ),
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Size',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF9AA09A),
-                            ),
-                          ),
-                          Text(
-                            'Medium',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Color(0xFF0E110E),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
+                      PlantInfo(label: 'Size', information: 'Medium'),
+                      SizedBox(
                         height: 44,
-                        width: 1,
-                        color: Colors.grey,
+                        child: VerticalDivider(
+                          color: Color(0xFFDBDAD9),
+                          thickness: 1,
+                        ),
                       ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Plant',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF9AA09A),
-                            ),
-                          ),
-                          Text(
-                            'Orchid',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Color(0xFF0E110E),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
+                      PlantInfo(label: 'Plant', information: 'Orchid'),
+                      SizedBox(
                         height: 44,
-                        width: 1,
-                        color: Colors.grey,
+                        child: VerticalDivider(
+                          color: Color(0xFFDBDAD9),
+                          thickness: 1,
+                        ),
                       ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Height',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF9AA09A),
-                            ),
-                          ),
-                          Text(
-                            '10.5"',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Color(0xFF0E110E),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
+                      PlantInfo(label: 'Height', information: '10.5”'),
+                      SizedBox(
                         height: 44,
-                        width: 1,
-                        color: Colors.grey,
+                        child: VerticalDivider(
+                          color: Color(0xFFDBDAD9),
+                          thickness: 1,
+                        ),
                       ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Humidnity',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Color(0xFF9AA09A),
-                            ),
-                          ),
-                          Text(
-                            '80%',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Color(0xFF0E110E),
-                            ),
-                          ),
-                        ],
-                      ),
+                      PlantInfo(label: 'Humidnity', information: '80%'),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('\$60.00'),
-                      Row(
-                        children: [
-                          Container(
-                            width: 30,
-                            height: 30,
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(5),
-                                bottomLeft: Radius.circular(5),
-                              ),
-                              border: Border.all(
-                                color: const Color.fromRGBO(154, 160, 154, 0.3),
-                              ),
-                            ),
-                            child: const Icon(
-                              Icons.remove,
-                            ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          '\$60.00',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Container(
-                            width: 30,
-                            height: 30,
-                            decoration: const BoxDecoration(
-                              color: Colors.transparent,
-                              border: Border.symmetric(
-                                horizontal: BorderSide(
-                                  color: Color.fromRGBO(154, 160, 154, 0.3),
+                        ),
+                        Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 30,
+                                height: 30,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(5),
+                                    bottomLeft: Radius.circular(5),
+                                  ),
+                                  border: Border.all(
+                                    color: const Color.fromRGBO(
+                                        154, 160, 154, 0.3),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.remove,
                                 ),
                               ),
                             ),
-                            child: const Center(child: Text('1')),
-                          ),
-                          Container(
-                            width: 30,
-                            height: 30,
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              borderRadius: const BorderRadius.only(
-                                topRight: Radius.circular(5),
-                                bottomRight: Radius.circular(5),
+                            Container(
+                              width: 30,
+                              height: 30,
+                              decoration: const BoxDecoration(
+                                color: Colors.transparent,
+                                border: Border.symmetric(
+                                  horizontal: BorderSide(
+                                    color: Color.fromRGBO(154, 160, 154, 0.3),
+                                  ),
+                                ),
                               ),
-                              border: Border.all(
-                                color: const Color.fromRGBO(154, 160, 154, 0.3),
+                              child: const Center(
+                                  child: Text(
+                                '1',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              )),
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 30,
+                                height: 30,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: const BorderRadius.only(
+                                    topRight: Radius.circular(5),
+                                    bottomRight: Radius.circular(5),
+                                  ),
+                                  border: Border.all(
+                                    color: const Color.fromRGBO(
+                                        154, 160, 154, 0.3),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.add,
+                                ),
                               ),
                             ),
-                            child: const Icon(
-                              Icons.add,
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 0,
+                        fixedSize: const Size(double.infinity, 56),
+                        backgroundColor: const Color(0xFF4B8E4B),
+                        foregroundColor: const Color(0xFFFFFFFF),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SvgPicture.asset(
+                            'lib/ui/assets/icons/shoppingcart.svg',
+                          ),
+                          const Text(
+                            'Add to Cart',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                            child: VerticalDivider(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Text(
+                            '\$60.00',
+                            style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      elevation: 0,
-                      fixedSize: const Size(335, 56),
-                      backgroundColor: const Color(0xFF4B8E4B),
-                      foregroundColor: const Color.fromRGBO(247, 247, 247, 1),
-                    ),
-                    child: const Text(
-                      'texto',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ],
