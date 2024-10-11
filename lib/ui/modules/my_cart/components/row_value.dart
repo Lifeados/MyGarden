cb963ad41c0a7e5e785eb9dd2ac87f477dcf76f1
Dione Santos <dionedev@gmail.com>
2024-10-11 16:54:03 -0300

    feat: adiciona componente row value

---
+++ lib/ui/modules/my_cart/components/row_value.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class RowValue extends StatelessWidget {
+  final String label;
+  final double value;
+  final TextStyle? labelStyle;
+  final TextStyle? valueStyle;
+
+  const RowValue({
+    super.key,
+    required this.label,
+    required this.value,
+    this.labelStyle,
+    this.valueStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          label,
+          style: labelStyle ??
+              const TextStyle(
+                fontSize: 16,
+              ),
+        ),
+        Text(
+          '\$$value',
+          style: valueStyle ??
+              const TextStyle(
+                fontSize: 16,
+              ),
+        ),
+      ],
+    );
+  }
+}
