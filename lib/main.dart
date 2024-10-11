a3d654ec20f62de63ae23f4940e66f9ec731aa56
Dione Santos <dionedev@gmail.com>
2024-10-11 18:05:20 -0300

    feat: adiciona pagina meu carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_garden/ui/modules/my_cart/my_cart_page.dart';
 import 'ui/modules/product_details/product_details_page.dart';
 import 'package:my_garden/ui/modules/signup/signup_page.dart';
 
@@ -15,11 +16,12 @@ class MyGardenApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/splash',
+      initialRoute: '/my_cart',
       routes: {
         '/splash': (context) => const SplashPage(),
         '/signup': (context) => const SignUpPage(),
         '/product/details': (context) => const ProductDetailsPage(),
+        '/my_cart': (context) => const MyCartPage(),
       },
     );
   }
