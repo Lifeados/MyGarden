96f04b52f86468f2f1b17492a6792cdc8716ff55
Dione Santos <dionedev@gmail.com>
2024-10-23 22:54:33 -0300

    feat: add secondary green color

--- lib/shared/utils/app_colors.dart
+++ lib/shared/utils/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryGreenColor = Color(0xff4B8E4B);
+  static const Color secondaryGreenColor = Color(0x124B8E4B);
   static const Color primaryWhiteColor = Color(0xffffffff);
   static const Color primaryDarkColor = Color(0xff0E110E);
   static const Color primaryGreyColor = Color(0x1A9AA09A);
