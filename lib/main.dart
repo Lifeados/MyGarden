9540b806f1a1f0f45128f0332354556cbe7c4523
aline amorim <alineamorimdev@gmail.com>
2024-10-29 18:04:57 -0300

    feat: add search plant page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:my_garden/shared/utils/app_colors.dart';
 import 'package:my_garden/ui/modules/cart/cart_page.dart';
 import 'package:my_garden/ui/modules/payment_method/payment_method_page.dart';
 import 'package:my_garden/ui/modules/onboarding/onboarding_page.dart';
+import 'package:my_garden/ui/modules/search_plant/search_plant.dart';
 import 'package:my_garden/ui/modules/shipping_address/shipping_address_page.dart';
 import 'ui/modules/product_details/product_details_page.dart';
 import 'package:my_garden/ui/modules/signup/signup_page.dart';
-
 import 'ui/modules/splash/splash_page.dart';
 
-void main() {
+late final CameraDescription firstCamera;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  firstCamera = cameras.first;
+
   runApp(const MyGardenApp());
 }
 
@@ -39,6 +46,7 @@ class MyGardenApp extends StatelessWidget {
         '/payment': (context) => PaymentMethodPage(),
         '/cart': (context) => const CartPage(),
         '/shipping/address': (context) => const ShippingAddressPage(),
+        '/search/plant': (context) => SearchPlantPage(camera: firstCamera),
       },
     );
   }
