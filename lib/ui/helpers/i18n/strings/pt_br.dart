de0d06864cc3b1e542c83dfb3e525e942a08e4db
Dione Santos <dionedev@gmail.com>
2024-12-16 23:26:02 -0300

    feat: add i18n support for multiple languages

---
+++ lib/ui/helpers/i18n/strings/pt_br.dart
@@ -0,0 +1,24 @@
+import 'package:my_garden/ui/helpers/i18n/strings/translation.dart';
+
+class PtBr implements Translation {
+  @override
+  String get loginPageTitle => "Seja bem-vindo 👋 \nao ";
+  @override
+  String get loginPageTitleAppName => "Feltrin App";
+  @override
+  String get loginPageSubTitle => "Faça login para continuar";
+  @override
+  String get emailLabel => "Email";
+  @override
+  String get passwordLabel => "Senha";
+  @override
+  String get forgotPassword => "Esqueceu a senha?";
+  @override
+  String get loginButton => "Login";
+  @override
+  String get alternativeLoginMessage => "Ou continue com a conta social";
+  @override
+  String get messageRegister => "Não tem uma conta?";
+  @override
+  String get registerTextButton => "Cadastrar";
+}
---
+++ lib/ui/helpers/i18n/strings/translation.dart
@@ -0,0 +1,12 @@
+abstract class Translation {
+  String get loginPageTitle;
+  String get loginPageTitleAppName;
+  String get loginPageSubTitle;
+  String get emailLabel;
+  String get passwordLabel;
+  String get forgotPassword;
+  String get loginButton;
+  String get alternativeLoginMessage;
+  String get messageRegister;
+  String get registerTextButton;
+}
