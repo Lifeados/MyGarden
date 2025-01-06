3ebf2b0b3336746a5993af6764d1eb3e864e64dd
Dione Santos <dionedev@gmail.com>
2025-01-06 19:37:59 -0300

    feat: component to customize the icons in the bottomNavigationBarItem

---
+++ lib/ui/modules/base/components/custom_bottom_navigation_icon.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:my_garden/shared/utils/app_colors.dart';
+
+class CustomBottomNavigationIcon extends StatelessWidget {
+  final String assetPath;
+  final bool isSelected;
+
+  const CustomBottomNavigationIcon({
+    required this.assetPath,
+    required this.isSelected,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color =
+        isSelected ? AppColors.primaryGreenColor : AppColors.secondaryDarkColor;
+
+    return SvgPicture.asset(
+      assetPath,
+      colorFilter: ColorFilter.mode(
+        color,
+        BlendMode.srcIn,
+      ),
+      height: 24,
+    );
+  }
+}
