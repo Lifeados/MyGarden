d4fd1e0b19c6d03a204f2996e0c75e9f63d4dc6e
Dione Santos <dionedev@gmail.com>
2025-01-08 18:16:42 -0300

    refactor: add product category

--- lib/domain/models/product_model.dart
+++ lib/domain/models/product_model.dart
@@ -1,27 +1,27 @@
-import 'package:my_garden/domain/models/product_variety_model.dart';
+import 'package:my_garden/domain/models/product_category_model.dart';
 
 class ProductModel {
-  final String product;
-  final List<ProductVarietyModel> varieties;
+  final String name;
+  final List<ProductCategoryModel> categories;
 
   ProductModel({
-    required this.product,
-    required this.varieties,
+    required this.name,
+    required this.categories,
   });
 
   factory ProductModel.fromMap(Map<String, dynamic> json) {
     return ProductModel(
-      product: json['product'],
-      varieties: (json['varieties'] as List)
-          .map((e) => ProductVarietyModel.fromJson(e))
+      name: json['name'],
+      categories: (json['categories'] as List)
+          .map((e) => ProductCategoryModel.fromJson(e))
           .toList(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'product': product,
-      'varieties': varieties.map((e) => e.toJson()).toList(),
+      'name': name,
+      'categories': categories.map((e) => e.toJson()).toList(),
     };
   }
 }
