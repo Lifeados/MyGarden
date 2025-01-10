b028c0d2d4872e42c5741d36aa2259870b5a61fc
Dione Santos <dionedev@gmail.com>
2025-01-10 09:55:17 -0300

    feat: add new color

--- lib/shared/utils/app_colors.dart
+++ lib/shared/utils/app_colors.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryGreenColor = Color(0xff4B8E4B);
-  static const Color secondaryGreenColor = Color(0x124B8E4B);
+  static const Color secondaryGreenColor = Color(0x1F22AF22);
   static const Color primaryWhiteColor = Color(0xffffffff);
   static const Color primaryDarkColor = Color(0xff0E110E);
+  static const Color secondaryDarkColor = Color(0xD10E110E);
   static const Color primaryGreyColor = Color(0x1A9AA09A);
   static const Color backgroundColor = Color(0xFFEEEEEE);
   static const Color shadowColor = Color(0x1A000000);
