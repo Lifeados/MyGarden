7a07c194bd7502fc7807220748a7327cecae1bae
Dione Santos <dionedev@gmail.com>
2025-01-10 18:23:57 -0300

    refactor: color palette from a neighboring app

--- lib/shared/utils/app_colors.dart
+++ lib/shared/utils/app_colors.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryGreenColor = Color(0xff4B8E4B);
-  static const Color secondaryGreenColor = Color(0x1F22AF22);
+  static const Color primaryGreenColor = Color(0xff1FC099);
+  static const Color secondaryGreenColor = Color(0x2F1FC098);
   static const Color primaryWhiteColor = Color(0xffffffff);
-  static const Color primaryDarkColor = Color(0xff0E110E);
+  static const Color primaryDarkColor = Color(0xff1E212B);
   static const Color secondaryDarkColor = Color(0xD10E110E);
-  static const Color primaryGreyColor = Color(0x1A9AA09A);
-  static const Color backgroundColor = Color(0xFFEEEEEE);
-  static const Color shadowColor = Color(0x1A000000);
+  static const Color primaryGreyColor = Color(0x6B9AA09A);
   static const Color secondaryGreyColor = Color(0xff9AA09A);
+  static const Color backgroundColor = Color(0xFFF1F4F6);
+  static const Color shadowColor = Color(0x1A000000);
   static const Color errorColor = Color(0xFFFF0000);
 }
